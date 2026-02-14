58f1b225f304cb3e8e36d28223116160a04ed393
Badgoomba <09.croquet_tippet@icloud.com>
2026-02-14 18:34:57 -0500

    Practice #2:  Lists & Dictionaries

--- venv/Variables&Datatypes.py
+++ venv/Variables&Datatypes.py
@@ -1,8 +1,9 @@
 # Practice: Create variables of different types
-age = 25
-height = 5.9
-name = "Alex"
-is_student = True
+age = 28
+height = 5.6
+name = "Michael"
+is_student = False
+
 
 # Print them type ()
 print(f"{name} is {type(name)}")
@@ -10,7 +11,7 @@ print(f"{age} is {type(age)}")
 print(f"{height} is {type(height)}")
 print(f"{is_student} is {type(is_student)}")
 
-# Final Results -- Tells you the data type 
+# Final Results -- The type class tells you the data type
 # Alex is <class 'str'> string since contained inside ""
 # 25 is <class 'int'> A whole number
 # 5.9 is <class 'float'> Decimal

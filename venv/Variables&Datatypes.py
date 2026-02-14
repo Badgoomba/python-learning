0225948c82b8c2a3cd626ac979588e8aa88c697f
Badgoomba <09.croquet_tippet@icloud.com>
2026-02-14 18:21:38 -0500

    Adding Practice #1: Variables & Data Types

---
+++ venv/Variables&Datatypes.py
@@ -0,0 +1,17 @@
+# Practice: Create variables of different types
+age = 25
+height = 5.9
+name = "Alex"
+is_student = True
+
+# Print them type ()
+print(f"{name} is {type(name)}")
+print(f"{age} is {type(age)}")
+print(f"{height} is {type(height)}")
+print(f"{is_student} is {type(is_student)}")
+
+# Final Results -- Tells you the data type 
+# Alex is <class 'str'> string since contained inside ""
+# 25 is <class 'int'> A whole number
+# 5.9 is <class 'float'> Decimal
+# True is <class 'bool'> Boolean since True or False
